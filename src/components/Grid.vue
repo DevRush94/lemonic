1aed7bcccf2261590c0cb69af6d11121c2e6760c
Rushabh <Rushabh.edu@gmail.com>
2023-09-10 21:38:37 +0530

    Spotify API Req

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -6,7 +6,78 @@ defineProps({
   }
 })
 </script>
+<script>
+var myToken;
+const clientId = '4875732b46fe4b2b8671c683ea012688';
+const clientSecret = 'f4490eb08e334cba9e0a02a472a59f1a'; // Keep this secret and safe, typically done server-side.
+const redirectUri = encodeURIComponent('http://127.0.0.1:5173/'); // Replace with your redirect URI
+const basicAuth = btoa(`${clientId}:${clientSecret}`);
+const scopes = encodeURIComponent('user-read-private user-read-email'); // Define your required scopes
 
+var url = new URL(window.location.href);
+var code = url.searchParams.get('code');
+if (!code) {
+  window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`;
+  url = new URL(window.location.href);
+  code = url.searchParams.get('code');
+  if (code) {
+    getAccessToken(code);
+  }
+}
+else {
+  url = new URL(window.location.href);
+  code = url.searchParams.get('code');
+  if (code) {
+    getAccessToken(code);
+  }
+}
+
+
+function getAccessToken(authorizationCode) {
+  const clientId = 'YOUR_CLIENT_ID';
+  const clientSecret = 'YOUR_CLIENT_SECRET'; // Keep this secret and safe, typically done server-side.
+  const redirectUri = 'http://localhost:3000/callback'; // Replace with your redirect URI
+
+  const basicAuth = btoa(`${clientId}:${clientSecret}`);
+
+  fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic ${basicAuth}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `grant_type=client_credentials&code=${authorizationCode}&redirect_uri=${encodeURIComponent(redirectUri)}`
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Access Token:', data.access_token);
+      myToken = data.access_token
+
+    })
+    .catch(error => {
+      console.error('Error fetching access token:', error);
+    });
+}
+
+// To get track
+// const accessToken = myToken // Replace with your token
+// const trackId = '5nTtCOCds6I0PHMNtqelas'; // Replace with your track ID
+
+// fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
+//   method: 'GET',
+//   headers: {
+//     'Authorization': `Bearer ${accessToken}`,
+//     'Content-Type': 'application/json'
+//   }
+// })
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('There was an error!', error);
+//   });
+</script>
 <template>
   <main>
     <div class="box">
