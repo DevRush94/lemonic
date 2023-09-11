8b894f69dfd87077600027d1731d85275d2e8da5
Rushabh <Rushabh.edu@gmail.com>
2023-09-11 22:28:41 +0530

    Animation Added

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="padder">
+  <main class="padder" :class="loadedClass" v-if="!loading">
     <div class="playList_layout" v-for="(playlist, playlistIndex) in subPlaylist" :key="'playlist-' + playlistIndex">
       <h3>{{ playlist.name }}</h3>
       <div class="dragger">
@@ -8,11 +8,14 @@
           @mouseover="startScrolling(playlist.id, 'left')"
           @mouseout="stopScrolling">
         </div>
-
         <ul class="box" v-bind:id="playlist.id">
-          <li class="grid" v-for="track in playlist.tracks.items" :key="track.id">
+          <li
+            class="grid"
+            v-for="(track, trackIndex) in playlist.tracks.items"
+            :key="track.id"
+            :class="{ load: hasLoaded(playlistIndex, trackIndex) }">
             <div class="song_box">
-              <img :src="track?.track?.album?.images?.[1]?.url" alt="Album cover">
+              <img :src="getAlbumCover(track)" alt="Album cover">
             </div>
           </li>
         </ul>
@@ -21,77 +24,81 @@
           @mouseover="startScrolling(playlist.id, 'right')"
           @mouseout="stopScrolling">
         </div>
-
       </div>
     </div>
   </main>
+  <div v-if="loading">Loading...</div>
 </template>
 
 <script>
 const clientId = '4875732b46fe4b2b8671c683ea012688';
 const clientSecret = 'f4490eb08e334cba9e0a02a472a59f1a';
 const basicAuth = btoa(clientId + ':' + clientSecret);
-const redirectUri = encodeURIComponent('http://127.0.0.1:5173/');
 
 export default {
   data() {
     return {
+      loading: true,
       mainData: null,
       playlistTracks: [],
       subPlaylist: [],
       scrolling: false,
       scrollDirection: null,
-      currentPlaylistId: null
+      currentPlaylistId: null,
+      loadTimes: {}
     };
   },
   async mounted() {
     const url = new URL(window.location.href);
-    // const code = url.searchParams.get('code');
     this.mainData = JSON.parse(localStorage.getItem('mainPlaylist'));
     this.playlistTracks = JSON.parse(localStorage.getItem('subPlaylist'));
 
+    if (!localStorage.getItem('token')) {
+      await this.getAccessToken();
+    }
 
-    // if (!code && !localStorage.getItem('token')) {
-    //   window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}`;
-    // } else {
-    //   window.history.replaceState({}, document.title, location.href.split("?")[0]);
-      if (!localStorage.getItem('token')) {
-        await this.getAccessToken();
-      }
-      if (!localStorage.getItem('mainPlaylist')) {
-        console.log("object", localStorage.getItem('mainPlaylist'));
-        await this.getFeaturedplaylists();
-      }
-      else {
-        await this.cacheLoad();
-      }
-    // }
+    if (!localStorage.getItem('mainPlaylist')) {
+      await this.getFeaturedplaylists();
+    } else {
+      this.cacheLoad();
+      this.loading = false;
+    }
   },
   methods: {
+    hasLoaded(playlistIndex, trackIndex) {
+      const key = `${playlistIndex}-${trackIndex}`;
+      const currentTime = Date.now();
+
+      // For tracks after the 10th, load by default
+      if (trackIndex >= 10) {
+        return true;
+      }
+
+      if (!this.loadTimes[key]) {
+        this.loadTimes[key] = currentTime + (playlistIndex * this.subPlaylist[playlistIndex].tracks.items.length + trackIndex) * 100;
+      }
+
+      return currentTime > this.loadTimes[key];
+    },
     startScrolling(playlistId, direction) {
       this.scrolling = true;
       this.scrollDirection = direction;
       this.currentPlaylistId = playlistId;
       this.scroll();
     },
-
     scroll() {
       if (!this.scrolling) {
         return;
       }
-
       const playlistElement = document.getElementById(this.currentPlaylistId);
       const scrollAmount = 10;  // Adjust for scroll speed
-
       if (this.scrollDirection === 'left') {
         playlistElement.scrollLeft -= scrollAmount;
       } else if (this.scrollDirection === 'right') {
         playlistElement.scrollLeft += scrollAmount;
       }
-
       requestAnimationFrame(this.scroll);
     },
-
     stopScrolling() {
       this.scrolling = false;
     },
@@ -119,11 +126,8 @@ export default {
       this.mainData = data;
       localStorage.setItem("mainPlaylist", JSON.stringify(this.mainData));
       console.log(data);
-
-      // Determine the number of playlists you want to fetch
-      const numberOfPlaylists = data.playlists.items.length; // You can change this number to fetch more or less
+      const numberOfPlaylists = data.playlists.items.length;
       let allPlaylistsData = [];
-
       for (let i = 0; i < numberOfPlaylists; i++) {
         const playlistResponse = await fetch(data.playlists.items[i].href, {
           method: 'GET',
@@ -135,15 +139,50 @@ export default {
         const playlistData = await playlistResponse.json();
         allPlaylistsData.push(playlistData);
       }
-
-      this.playlistTracks = allPlaylistsData[0].tracks.items; // Using the first playlist's tracks as an example
+      this.playlistTracks = allPlaylistsData[0].tracks.items;
       localStorage.setItem("subPlaylist", JSON.stringify(allPlaylistsData));
       this.cacheLoad()
     },
     async cacheLoad() {
       this.mainData = JSON.parse(localStorage.getItem('mainPlaylist'));
       this.subPlaylist = JSON.parse(localStorage.getItem('subPlaylist'));
-      console.log(this);
+
+      this.loading = false;
+
+      // Initialize loadTimes
+      for (let playlistIndex = 0; playlistIndex < this.subPlaylist.length; playlistIndex++) {
+        for (let trackIndex = 0; trackIndex < 10 && trackIndex < this.subPlaylist[playlistIndex].tracks.items.length; trackIndex++) {
+          const key = `${playlistIndex}-${trackIndex}`;
+          this.loadTimes[key] = Date.now() + ((playlistIndex * this.subPlaylist[playlistIndex].tracks.items.length + trackIndex) * 100);
+        }
+      }
+
+      // Start cascading effect
+      this.$nextTick(() => {
+        this.startCascadingEffect();
+      });
+    },
+
+    startCascadingEffect() {
+      const timePerTrack = 50;  // 2000ms for 10 tracks
+      for (let playlistIndex = 0; playlistIndex < this.subPlaylist.length; playlistIndex++) {
+        for (let trackIndex = 0; trackIndex < 10 && trackIndex < this.subPlaylist[playlistIndex].tracks.items.length; trackIndex++) {
+          const key = `${playlistIndex}-${trackIndex}`;
+          // Calculating the delay for each track
+          const delay = (playlistIndex * 10 + trackIndex) * timePerTrack;
+
+          setTimeout(() => {
+            this.loadTimes[key] = 0.1;  // Set to past time to trigger the effect
+          }, delay);
+        }
+      }
+    },
+
+    getAlbumCover(track) {
+      if (track && track.track && track.track.album && track.track.album.images && track.track.album.images[1]) {
+        return track.track.album.images[1].url;
+      }
+      return '';
     }
   }
 };
